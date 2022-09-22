809f3a89b64a926ba14774d8f564b41cdc2085ec
Vyacheslav <vyacheslav@izzqz.me>
2022-09-22 19:37:31 +0300

    Add new aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,18 @@ set -e SSH_AUTH_SOCK
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
 
 nvm use default --silent
+
 alias l="ls"
 alias ls="exa --icons -TL 1"
 alias la="ls --all"
 alias ll="ls -lF --all"
 alias v="nvim"
-alias rm="safe-rm"
+alias cat="batcat"
+alias restart="source ~/.config/fish/config.fish"
+alias i="sudo aptitude install -y"
+alias get="aria2c"
 
 if status is-interactive
-    # hemlo
+    rbenv init - fish | source &
 end
 
